f634b2576231eaff029379d630c509ed1792f771
Bart van de Biezen <bart@bartvandebiezen.com>
2015-03-30 23:02:25 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# kirby-v2-picture-tag
-Picture element for Kirbytext
+# Picture tag for Kirby 2
+
+An extension for KirbyText making the picture element available as a tag. Although you can use the picture element for all responsive images. It is advised to only use it for art directional purposes. See [Responsive Images Community Group](http://responsiveimages.org) for more information about the picture element.
+
+## Installation
+
+1. Copy ‘picture.php’ inside ‘tags’ to Kirby’s ‘site/tags/‘ folder.
+2. Make sure you have picturefill.js, if not:
+2.1. Copy ‘picturefill-2.2.0.min.js’ inside ‘assets/javascripts’ to Kirby’s ‘assets/javascripts/‘ folder or [download the latest version](http://scottjehl.github.io/picturefill/).
+2.2. Place ```php
+<?php echo js(‘assets/javascripts/picturefill-2.2.0.min.js’) ?>``` in your footer file above ```HTML </body>```.
+
+## Usage
+
+### Attributes
+
+You can use the picture tag in your markdown files for Kirby as follows:
+
+(picture: filename extension: png class: my-classname caption: a figcaption alt: alt text width: 100px height: 200px)
+
+Be careful, you need to write the filename without an extension. The extension is written as a separate attribute. When you do not use the extension attribute, the extension will be ‘jpg’.
+
+### File names
+
+Naming files should be done as follows:
+
+- filename.jpg (fall back)
+- filename~palm.jpg
+- filename~palm@2x.jpg
+- filename~lap.jpg
+- filename~lap@2x.jpg
+- filename~desk.jpg
+- filename~desk@2x.jpg
+
+The naming convention is based on naming convention for Apple's iOS. If you want, you can change the naming convention in ‘picture.php’.
+
+### Viewport widths
+
+The 'palm' image will be visible when the viewport is smaller than 460px. The 'lap' image will be visible between 461px and 1024px. The 'desk' image will be visible when the viewport is wider than 1025px. If you want, you can change these media queries in 'picture.php'.
---
+++ tags/picture.php
@@ -0,0 +1,106 @@
+<?php
+
+kirbytext::$tags['picture'] = array (
+
+	'attr' => array (
+		'extension',
+		'class',
+		'caption',
+		'alt',
+		'width',
+		'height',
+	),
+
+	'html' => function ($tag) {
+
+		// Place attributes in variables
+		$name      = $tag->attr('picture');
+		$extension = '.' . $tag->attr('extension', 'jpg'); // If no type is given, extension will be '.jpg'.
+		$class     = $tag->attr('class');
+		$caption   = $tag->attr('caption');
+		$alt       = $tag->attr('alt');
+		$width     = $tag->attr('width');
+		$height    = $tag->attr('height');
+
+		// Settings. You could change this if needed for your project. Modifiers are inspired by Apple's iOS.
+		$image1Modifier   = '~palm';
+		$image1UpperBound = '460px';
+		$image2Modifier   = '~lap';
+		$image2LowerBound = '461px';
+		$image2UpperBound = '1024px';
+		$image3Modifier   = '~desk';
+		$image3LowerBound = '1025px';
+		$retinaModifier   = '@2x';
+
+		// State possible files
+		$image        = $tag->page()->file($name . $extension);
+		$image1       = $tag->page()->file($name . $image1Modifier . $extension);
+		$image1Retina = $tag->page()->file($name . $image1Modifier . $extension . $retinaModifier);
+		$image2       = $tag->page()->file($name . $image2Modifier . $extension);
+		$image2Retina = $tag->page()->file($name . $image2Modifier . $extension . $retinaModifier);
+		$image3       = $tag->page()->file($name . $image3Modifier . $extension);
+		$image3Retina = $tag->page()->file($name . $image3Modifier . $extension . $retinaModifier);
+
+		// Get the URL when file exist
+		if ($image)        { $url = $image->url(); }
+		if ($image1)       { $url1 = $image1->url(); }
+		if ($image1Retina) { $url1Retina = $image1Retina->url(); }
+		if ($image2)       { $url2 = $image2->url(); }
+		if ($image2Retina) { $url2Retina = $image2Retina->url(); }
+		if ($image3)       { $url3 = $image3->url(); }
+		if ($image3Retina) { $url3Retina = $image3Retina->url(); }
+
+		// Starting figure
+		$buffer = '<figure';
+		if ($class) { $buffer .= ' class="' . $class . '"'; }
+		$buffer .= '>' . "\n";
+
+		// Starting picture
+		$buffer .= '<picture>' . "\n";
+
+		// Source for palm (e.g. small)
+		if ($image1 or $image1Retina) { $buffer .= '<source srcset="'; }
+		if ($image1) { $buffer .= $url1; }
+		if ($image1 and $image1Retina) { $buffer .= ', '; }
+		if ($image1Retina) { $buffer .= $url1Retina . ' 2x'; }
+		if ($image1 or $image1Retina) { $buffer .= '" media="(max-width: ' . $image1UpperBound . ')">' . "\n"; }
+
+		// Source for lap (e.g. medium)
+		if ($image2 or $image2Retina) { $buffer .= '<source srcset="'; }
+		if ($image2) { $buffer .= $url2; }
+		if ($image2 and $image2Retina) { $buffer .= ', '; }
+		if ($image2Retina) { $buffer .= $url1Retina . ' 2x'; }
+		if ($image2 or $image2Retina) { $buffer .= '" media="(min-width: ' . $image2LowerBound . ') and (max-width: ' . $image2UpperBound .')">' . "\n"; }
+
+		// Source for desk (e.g. large)
+		if ($image3 or $image3Retina) { $buffer .= '<source srcset="'; }
+		if ($image3) { $buffer .= $url3; }
+		if ($image3 and $image3Retina) { $buffer .= ', '; }
+		if ($image3Retina) { $buffer .= $url3Retina . ' 2x'; }
+		if ($image3 or $image3Retina) { $buffer .= '" media="(min-width: ' . $image3LowerBound . ')">' . "\n"; }
+
+		// Use image without modifiers as fall back.
+		$buffer .= '<img';
+		if ($width) { $buffer .= ' width="' . $width . '"'; }
+		if ($height) { $buffer .= ' height="' . $height . '"'; }
+		if ($image) { $buffer .= ' src="' . $url . '"'; } else { $buffer .= ' src="' . $url3 . '"'; }
+		if ($image3 and $image3Retina) { $buffer .= ' srcset="' . $url3 . ', ' . $url3Retina . ' 2x"'; }
+
+		if ($alt) { $buffer .= ' alt="' . $alt . '"'; }
+		$buffer .= '>' . "\n";
+
+		// Ending picture
+		$buffer .= '</picture>' . "\n";
+
+		// Optional figure caption
+		if ($caption) { $buffer .= '<figcaption>' . $caption . '</figcaption>' . "\n"; }
+
+		// Ending figure
+		$buffer .= '</figure>' . "\n";
+
+		// Output buffer
+		return $buffer;
+	}
+);
+
+?>
